c8b4868ce72c0161b41b325b2f78f3c4c8b853df
gpsagar123 <gpsagar123@gmail.com>
2023-02-27 13:49:38 +0530

    Device File is the Model File

---
+++ DeviceFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole.Models
+{
+	public class DeviceFile
+	{
+		public bool IsDirectory { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+		public string Path { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Contains file path for displaying in WebBrowser
+		/// .log, .csv and.xml are restrict file extensions and doesn't launch within WebBrowser 
+		/// A copy of file is created with .txt extension and its file path will be saved in the property
+		/// </summary>
+		public string FilePath { get; set; } = string.Empty;
+
+		public List<DeviceFile> Contents { get; set; } = new List<DeviceFile>();
+
+		public bool HasContents { get { return Contents?.Any() ?? false; } }
+
+		public bool CanView { get { return !string.IsNullOrEmpty(Name) ? viewFileExtensions.Contains(System.IO.Path.GetExtension(Name).ToLower()) : false; } }
+
+		public bool CanViewInGrok { get { return string.Equals(Name, "DeviceHistory.db", StringComparison.OrdinalIgnoreCase); } }
+
+		private string[] viewFileExtensions => new string[] { ".txt", ".csv", ".xml", ".log", ".ivd", ".trn" };
+	}
+}
